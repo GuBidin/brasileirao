7c29c1b8460a37ec6604a4b6f3fd6dcb69f23f9b
GuBidin <gustavobidin89@gmail.com>
2026-07-28 18:02:59 -0300

    convertendo a data, criando coluna ano e extraindo o ano da data, criando coluna resultado e realizando logica para ver o vencedor perdedor ou empate, criando coluna resultado e exibindo as colunas

--- explorar.py
+++ explorar.py
@@ -18,3 +18,34 @@ print(df.head()) #df.head() é um método do DataFrame que retorna as primeiras 
 # Imprimir informações gerais
 print("\nInformações gerais:")
 print(df.info()) #df.info() é um método do DataFrame que retorna informações gerais sobre o DataFrame, como o número de linhas, o número de colunas, o tipo de dados de cada coluna, e a quantidade de valores não nulos em cada coluna.
+
+
+#Convertendo a coluna data de texto para data de verdade
+df["data"] = pd.to_datetime(df["data"]) #pd.to_datetime() é uma função do pandas que converte uma coluna de texto para o tipo datetime. Reatribuímos o resultado de volta para df["data"].
+
+#Criando uma nova coluna ano, extraindo o ano da data
+df["ano"] = df["data"].dt.year #df["data"].dt.year é um atributo do pandas que retorna o ano de uma coluna datetime. Criamos uma nova coluna "ano" e atribuímos o resultado de df["data"].dt.year a ela.
+
+#Criando uma coluna resultado que diga se foi "Vitória mandante", "Vitória visitante" ou "Empate"
+def resultado(row): #definimos uma função chamada resultado que recebe uma linha do DataFrame
+
+    if row["mandante_Placar"] > row["visitante_Placar"]: #se o placar do mandante for maior que o placar do visitante
+        return "Vitória mandante" #retorna "Vitória mandante"
+    elif row["mandante_Placar"] < row["visitante_Placar"]: #se o placar do mandante for menor que o placar do visitante
+        return "Vitória visitante" #retorna "Vitória visitante"
+    else: #se não for nenhuma das duas condições acima, ou seja, se for empate
+        return "Empate" #retorna "Empate"
+#Criando coluna resultado
+df["resultado"] = df.apply(resultado, axis=1) #aplicamos a função resultado a cada linha do DataFrame e atribuímos o resultado à nova coluna "resultado"
+
+#Exibindo as colunas data, ano, mandante_Placar, visitante_Placar e resultado
+print(df[
+    [
+        "data", 
+        "ano",
+        "mandante_Placar", 
+        "visitante_Placar", 
+        "resultado"
+    ]
+].head(10)
+)
